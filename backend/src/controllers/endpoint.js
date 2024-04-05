41800a6d81935e4cf98f7ac864bb66420edc26b2
SebiTCR <Ssebi832@outlook.com>
2024-04-05 18:53:35 +0300

    Added placeholders

---
+++ backend/src/controllers/endpoint.js
@@ -0,0 +1,31 @@
+let placeholder = {
+    status: [
+        {
+            time: Date.now(),
+            status: 0
+        },
+        {
+            time: Date.now(),
+            status: 0
+        },
+        {
+            time: Date.now(),
+            status: 0
+        }
+    ]
+}
+
+function sendConnection (req, res){
+    //TODO: fetch website status
+    res.send(placeholder)
+}
+
+function registerLink(req, res){
+    //TODO: Register link in the DB
+    let link = req.body.link;
+    console.log(link)
+    res.send({msg: "Link registered", link: link})
+}
+
+
+module.exports = {sendConnection, registerLink}
---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const endpointCtrl = require('./controllers/endpoint')
+const bp = require('body-parser')
+
+
+app.use(cors())
+app.use(bp.json())
+
+app.get('/api/link', endpointCtrl.sendConnection)
+app.post('/api/link', endpointCtrl.registerLink)
+
+app.listen(8085, ()=>{
+    console.log("Server started listening on 8085!")
+})
