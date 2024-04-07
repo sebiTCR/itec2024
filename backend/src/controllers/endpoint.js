70a4e30436dc1cdf81bcac1d473d4ee515d73cf8
SebiTCR <Ssebi832@outlook.com>
2024-04-07 08:21:47 +0300

    Added CRUD operations for endpoints

--- backend/src/controllers/endpoint.js
+++ backend/src/controllers/endpoint.js
@@ -28,4 +28,43 @@ async function getEntries(req, res){
 }
 
 
-module.exports = {sendConnection, registerLink, getEntries}
+async function remove(req, res){
+    const endpoint = req.body.endpoint
+    // let results = await db.Endpoint.find({endpoint: endpoint})
+    // if(!results)
+    //     res.send({msg: "No entry found"})
+    //     return
+    await db.Endpoint.findOneAndDelete({endpoint: endpoint})
+    res.send({msg: `Removed ${endpoint}`})
+}
+
+async function update(req, res){
+    const endpoint = req.body.endpoint
+    const newEndpoint = req.body.new_endpoint
+    if(newEndpoint == " "){
+        res.send({msg: "The new endpoint cannot be empty!"})
+        return
+    }
+
+    await db.Endpoint.findOneAndUpdate({endpoint: endpoint}, {endpoint: newEndpoint})
+    res.send({msg: `Updated ${endpoint} to ${newEndpoint}`})
+}
+
+
+async function report(req, res){
+    //TODO: Implement mail sender
+    const endpoint = req.body.endpoint
+    await db.Endpoint.findOneAndUpdate({endpoint: endpoint}, {reported: true})
+    res.send({msg: `Reported ${endpoint}`})
+
+}
+
+async function fix(req, res){
+    //TODO: Implement credential check
+    const endpoint = req.body.endpoint
+    await db.Endpoint.findOneAndUpdate({endpoint: endpoint}, {reported: true})
+    res.send({msg: `Fixed ${endpoint}`})
+}
+
+
+module.exports = {sendConnection, registerLink, getEntries, remove, update, report, fix}
--- backend/src/server.js
+++ backend/src/server.js
@@ -11,9 +11,13 @@ app.use(bp.json())
 
 app.get('/api/endpoint', endpointCtrl.sendConnection)
 app.post('/api/endpoint', endpointCtrl.registerLink)
+app.put('/api/endpoint', endpointCtrl.update)
+app.delete('/api/endpoint/', endpointCtrl.remove)
+app.post('/api/endpoint/report', endpointCtrl.report)
+app.patch('/api/endpoint/report', endpointCtrl.fix)
 app.get('/api/endpoints', endpointCtrl.getEntries)
 
-sw.update()
+sw.setup()
 
 app.listen(8085, ()=>{
     console.log("Server started listening on 8085!")
